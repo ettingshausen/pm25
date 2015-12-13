a6e4ea14cc397c5ae68e1217a44e268e55355fbb
ettingshausen <ettingshausen@outlook.com>
2015-12-14 02:58:53 +0800

    import to Android Studio and fix some bugs

---
+++ app/src/main/java/me/ninjachen/util/APIClient.java
@@ -0,0 +1,58 @@
+package me.ninjachen.util;
+
+import android.util.Log;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public final class APIClient {
+    private static final String LogTag = "pm25";
+    private static HttpClient mHttpClent = new DefaultHttpClient();
+
+    //http post
+    public String post(String uri, String param) {
+        Log.d(LogTag, String.format("request post message:%s",
+                param));
+        String result = null;
+        HttpPost httpPost = new HttpPost(uri);
+        try {
+            ArrayList<BasicNameValuePair> list = new ArrayList<BasicNameValuePair>();
+            list.add(new BasicNameValuePair("message", param));
+            httpPost.setEntity(new UrlEncodedFormEntity(list, "UTF-8"));
+            HttpResponse httpResponse = mHttpClent.execute(httpPost);
+            Log.d(LogTag, String.format("post StatusCode:%s", httpResponse
+                    .getStatusLine().getStatusCode()));
+            result = EntityUtils.toString(httpResponse.getEntity());
+            Log.d(LogTag, String.format("post HttpResponse:%s",
+                    result));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    //http-get
+    public String request(String param) {
+        Log.d(LogTag,
+                String.format("request get uri:%s", param));
+        HttpGet httpGet = new HttpGet(param);
+        try {
+            HttpResponse httpResponse = mHttpClent.execute(httpGet);
+            Log.d(LogTag, String.format("get StatusCode:%s", httpResponse
+                    .getStatusLine().getStatusCode()));
+            return EntityUtils.toString(httpResponse.getEntity());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
